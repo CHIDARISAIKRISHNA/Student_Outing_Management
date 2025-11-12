1e51637e1d4560ff7fe1f29bdd23dab2029598d8
KORUPOLU BHARGAV <165929453+Bhargavk055@users.noreply.github.com>
2025-11-12 20:49:41 +0530

    Add text file

---
+++ text.py
@@ -0,0 +1,17 @@
+
+text = input("Enter a string: ")
+
+vowels = 0
+consonants = 0
+
+vowel_set = "aeiouAEIOU"
+
+for char in text:
+    if char.isalpha():  
+        if char in vowel_set:
+            vowels += 1
+        else:
+            consonants += 1
+
+print("Number of vowels:", vowels)
+print("Number of consonants:", consonants)
